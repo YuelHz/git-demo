27998fbe77f6f9bc719790664d97685d4f798c6f
YuelHz <1977559097@qq.com>
2021-04-06 19:05:55 +0800

    boot配置文件对象

--- spring_boot_helloworld/src/main/java/com/yuel/DemoApplication.java
+++ spring_boot_helloworld/src/main/java/com/yuel/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.yuel;
 
+import com.yuel.config.FoodConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @SpringBootApplication
  * 声明是一个入口类  并且是springboot的入口类
+ *
+ * @EnableConfigurationProperties
+ * 告诉主程序入口类  要自动引入配置文件
+ * 配置文件的类作为他的参数
  */
 @SpringBootApplication
+@EnableConfigurationProperties(FoodConfig.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
--- spring_boot_helloworld/src/main/java/com/yuel/controller/HelloController.java
+++ spring_boot_helloworld/src/main/java/com/yuel/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.yuel.controller;
 
 import com.yuel.bean.Food;
+import com.yuel.config.FoodConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +11,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @Value("${food.rice}")
-    private String rice;
-
-    @Value("${food.meat}")
-    private String meat;
+//    @Value("${food.rice}")
+//    private String rice;
+//
+//    @Value("${food.meat}")
+//    private String meat;
+    @Autowired
+    private FoodConfig foodConfig;
 
 
     @RequestMapping("/json")
     @ResponseBody
     public Food hello(){
         Food food = new Food();
-        food.setMeat(meat);
-        food.setRice(rice);
+        food.setMeat(foodConfig.getMeat());
+        food.setRice(foodConfig.getRice());
         return food;
     }
 }
